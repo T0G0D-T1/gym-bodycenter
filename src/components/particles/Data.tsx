50d52ee9d01d23237ecedcef6dae6b14225dbdc9
T0G0D-T1 <parra2002@gmail.com>
2026-04-27 01:02:09 -0400

    SIU

---
+++ src/components/particles/Data.tsx
@@ -0,0 +1,206 @@
+export const NavLinks = [
+    {
+        name: "Home",
+        url: "/"
+    },
+    {
+        name: "Sobre nosotros",
+        url: "/about"
+    },
+    {
+        name: "Horarios",
+        url: "/horario"
+    },
+    {
+        name: "Galeria",
+        url: "/galeria"
+    },
+    {
+        name: "Noticias",
+        url: "/noticias"
+    },
+    {
+        name: "Contacto",
+        url: "/contacto"
+    },
+];
+
+
+
+export const HeroTexts = [
+    {
+        Heading: "¡Desata tu fuerza interior!",
+        Paragraph: "¡Bienvenido a Body Center, donde el fitness se encuentra con la diversión! ¡Prepárate para desatar tu fuerza interior y transformar tu vida!",
+        Button: "¡Únete ahora!"
+    },
+    {
+        Heading: "¡Lleva tu entrenamiento a otro nivel!",
+        Paragraph: "¡Supera tus límites y logra grandes resultados! Súmate a una comunidad que te apoya y comienza un camino de crecimiento personal y motivación.",
+        Button: "¡Inscríbete hoy!"
+    },
+    {
+        Heading: "¡El fitness se disfruta con nosotros!",
+        Paragraph: "En nuestro gimnasio, entrenar no es solo una rutina, ¡es una experiencia! Súmate a esta aventura fitness, donde la diversión y los resultados van de la mano.",
+        Button: "¡Inscríbete ahora!"
+    }
+]
+
+
+export const AboutTexts = {
+    firstText: "Sobre Body Center",
+    secondText: "Bienvenido",  // debajo de "bienvenido" sale "fade" arreglar luego.
+    caption: "¡Bienvenido a Body Center, donde el fitness se une a la comunidad y tu transformación se hace realidad!",
+    paragraph1: "En Body Center somos mucho más que un gimnasio; somos una comunidad fitness apasionada y cercana, dedicada a ayudarte a cumplir tus metas de salud y bienestar. Nuestra misión es crear un ambiente positivo y motivador que te inspire a superarte, llevar una vida saludable y descubrir tu mejor versión.",
+}
+
+export const OfferTexts = {
+    firstText: "Lo que hacemos",
+    secondText: "Todo lo que ofrecemos",
+    list: [
+        {
+            listCaption: "Programas para bajar de peso",
+            text: "Con nuestro enfoque personalizado, asesoría experta y resultados comprobados, te ayudaremos a alcanzar tus metas fitness. Da hoy el primer paso hacia un cambio en tu cuerpo y estilo de vida.",
+        },
+        {
+            listCaption: "Programas de volumen",
+            text: "Nuestros entrenadores expertos te guiarán con rutinas personalizadas para que logres tus objetivos de musculación de forma eficiente y segura.",
+        }
+        , {
+            listCaption: "Clases especiales variadas",
+            text: "Lleva tu entrenamiento a otro nivel con nuestras clases especiales segun tus requisitos, donde superarás tus límites y lograrás excelentes resultados físicos.",
+        }
+    ],
+}
+
+export const MembershipPlans = {
+    firstText: "Planes y precios",
+    secondText: "Membresías",
+    cards: [
+        {
+            amount: 8000,
+            duration: "día",
+            caption: "Pase diario",
+            benefits: ["Acceso ilimitado al gimnasio", "Coach durante todo tu entrenamiento", "Acceso a evaluacion medica", "Orientacion en tu entrenamiento"]
+        },
+        {
+            amount: 35990,
+            duration: "mes",
+            caption: "Acceso mensual ilimitado",
+            benefits: ["Acceso ilimitado al gimnasio durante un mes", "Coach durante todo tu entrenamiento", "Acceso a evaluacion medica", "Acceso a In-Body"]
+        },
+        {
+            amount: 135000,
+            duration: "mes",
+            caption: "Plan trimestral",
+            benefits: ["Acceso ilimitado al gimnasio durante un mes", "Coach durante todo tu entrenamiento", "Acceso a evaluacion medica", "Acceso a In-Body"]
+        }
+    ]
+}
+
+export const ContactTexts = {
+    firstText: "Llamanos o envianos un Whatsapp",
+    phone: "+56947977983",
+    paragraph: "En nuestro gimnasio puedes contactar a cualquiera de nuestros entrenadores para resolver cualquier tipo de duda. ¡Tu bienestar es nuestra prioridad!",
+    button: "¡Envianos un mensaje!" // arreglar url wa.me/+56947977983
+}
+
+export const TestimonialTexts = {
+    firstText: "Nuestros testimonios",
+    secondText: "Lo que dicen nuestros clientes",
+    feedBacks: [
+        {
+            text: "Llevo seis meses como miembro, y la comunidad cercana del gimnasio junto a sus entrenadores capacitados me han ayudado a superar mis metas fitness.",
+            person: "Nicolas Contreras",
+            type: "Cliente"
+        },
+        {
+            text: "Desde que me uní al gimnasio, he notado grandes cambios en mi energía y condición física. El ambiente es motivador, los entrenadores siempre están dispuestos a ayudar y cada entrenamiento se disfruta..",
+            person: "Alonso Hernandez",
+            type: "Cliente"
+        },
+        {
+            text: "Entrenar acá ha sido una excelente experiencia. Las instalaciones son cómodas, el ambiente es muy bueno y el apoyo de los entrenadores me ha ayudado a avanzar más rápido hacia mis objetivos.",
+            person: "Jose Rodriguez",
+            type: "Cliente"
+        }
+    ],
+}
+
+export const BlogTexts = {
+    firstText: "Get informed",
+    secondText: "Our Latest News",
+    blogNews: [
+        {
+            caption: "Yoga",
+            title: "Do your self realizations quickly fade",
+            paragraph: "Discover the transformative power of yoga as we explore its numerous physical and mental benefits. From increased flexibility and strength to reduced stress and enhanced mindfulness, our yoga classes cater to practitioners of all levels. Join us on the mat and experience the harmony of mind, body, and soul.",
+            time: "1m",
+            author: "Ralph Cruz",
+            comments: "21"
+        },
+        {
+            caption: "Trainers & Equipment",
+            title: "Little Things Do make a difference",
+            paragraph: "At our gym, we take pride in our exceptional team of expert trainers who are passionate about guiding you on your fitness journey. With their knowledge and personalized approach, you'll receive the support you need to achieve your fitness goals effectively. Additionally, we offer state-of-the-art equipment that's designed to elevate your workouts and provide a holistic fitness experience like no other.",
+            time: "15m",
+            author: "Bruce Charles",
+            comments: "45"
+        },
+        {
+            caption: "Procrastination",
+            title: "The Time is Now",
+            paragraph: "Procrastination can be a common obstacle on the path to achieving our goals. In this blog, we delve into effective strategies to overcome procrastination and increase productivity. From time management techniques to staying motivated, we're here to help you take charge of your goals and embrace a more focused and fulfilling life. It's time to seize the day and make your dreams a reality!",
+            time: "19m",
+            author: "Steve Wagner",
+            comments: "55"
+        }
+    ]
+}
+
+export const FooterTexts = {
+    underLogoText: "We are more than just a gym; we are a passionate and supportive fitness family dedicated to helping you achieve your health and wellness goals. Our mission is to create a positive and empowering environment that inspires you to challenge yourself, embrace a healthy lifestyle, and discover the best version of yourself.",
+    quickLinks: {
+        caption: "Quick Links",
+        links: [
+            {
+                name: "Home",
+                url: "/"
+            },
+            {
+                name: "About Us",
+                url: "/about"
+            },
+            {
+                name: "Schedule",
+                url: "/schedule"
+            },
+            {
+                name: "Gallery",
+                url: "/gallery"
+            },
+            {
+                name: "News",
+                url: "/news"
+            },
+            {
+                name: "Contacts",
+                url: "/contact"
+            },
+        ]
+    },
+    contacts: {
+        caption: "Quick Contacts",
+        names: [
+            {
+                name: "11567 Santa Monica Blvd, Los Angeles, CA 90025, United States",
+            },
+            {
+                name: "gym_nex@example.com",
+            },
+            {
+                name: "+1 424-248-8496",
+            }
+        ]
+    },
+    copyright: "Copyright 2023. All Right Reserved"
+}
